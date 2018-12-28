98c4a06b018ac07b82e59a018fb4d005739081a8
Ivaylo Tabakov <ivo_tabako@yahoo.com>
2018-12-28 11:26:53 +0100

    initial commit

---
+++ program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 202; i++)
+            {
+                var bytes = DownloadFile($"https://vs1.coursehunters.net/mosh-react-mastering/lesson{i}.mp4").GetAwaiter().GetResult();
+                File.WriteAllBytes($"C:\\mosh\\lesson{i}.mp4", bytes);
+            }
+        }
+
+
+        public static async Task<byte[]> DownloadFile(string url)
+        {
+            using (var client = new HttpClient())
+            {
+
+                using (var result = await client.GetAsync(url))
+                {
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return await result.Content.ReadAsByteArrayAsync();
+                    }
+
+                }
+            }
+            return null;
+        }
+    }
+}
